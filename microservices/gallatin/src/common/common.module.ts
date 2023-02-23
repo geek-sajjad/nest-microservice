9de0f2afad4a2f8ffbd99873b1b015c6a723280c
Sajad Sohrabi <sajjad.sohrabi2@gmail.com>
2023-02-23 01:12:38 -0800

    update database connection setting

--- microservices/gallatin/src/common/common.module.ts
+++ microservices/gallatin/src/common/common.module.ts
@@ -11,7 +11,8 @@ import * as Joi from 'joi';
     ConfigModule.forRoot({
       load: [
         registerAs('database', () => ({
-          host: process.env?.DATABASE_HOST ?? 'postgresql://localhost:5432',
+          host: process.env?.DATABASE_HOST ?? 'localhost',
+          port: process.env?.DATABASE_PORT ?? '5432',
           name: process.env?.DATABASE_NAME ?? 'task-manager',
           user: process.env?.DATABASE_USER ?? 'postgres',
           password: process?.env.DATABASE_PASSWORD,
--- microservices/gallatin/src/common/database/services/database.options.service.ts
+++ microservices/gallatin/src/common/database/services/database.options.service.ts
@@ -7,6 +7,7 @@ import { IDatabaseOptionsService } from '../interfaces/database.options-service.
 export class DatabaseOptionsService implements IDatabaseOptionsService {
   private readonly host: string;
   private readonly database: string;
+  private readonly port: number;
   private readonly user: string;
   private readonly password: string;
   private readonly debug: boolean;
@@ -16,6 +17,7 @@ export class DatabaseOptionsService implements IDatabaseOptionsService {
   constructor(private readonly configService: ConfigService) {
     this.env = this.configService.get<string>('app.env');
     this.host = this.configService.get<string>('database.host');
+    this.port = this.configService.get<number>('database.port');
     this.database = this.configService.get<string>('database.name');
     this.user = this.configService.get<string>('database.user');
     this.password = this.configService.get<string>('database.password');
@@ -35,11 +37,15 @@ export class DatabaseOptionsService implements IDatabaseOptionsService {
 
     const typeormOptions: Record<string, any> = {
       type: 'postgres',
-      url: uri,
+      host: this.host,
+      port: this.port,
+      username: this.user,
+      password: this.password,
       retryDelay: 5000,
       logging: this.env === 'production' ? false : this.debug ? true : false,
       keepConnectionAlive: this.env === 'production' ? true : false,
-      synchronize: this.env === 'production' ? false : true,
+      // synchronize: this.env === 'production' ? false : true,
+      synchronize: false,
       entities: [
         __dirname +
           '/../../../{common,modules}/**/**/repository/entities/*.entity{.ts,.js}',
